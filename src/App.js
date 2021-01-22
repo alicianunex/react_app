abfa5bf1104a9bc285efa594c503d7bb884f7de3
alicianunezisaac <alicianunez10492@gmail.com>
2021-01-22 22:07:10 +0100

    getting started with components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>Hello World</h1>
+      <Header title="React App" />
     </Fragment>
   );
 };
