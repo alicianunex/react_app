ca490aee82fa3e5ab38acdcea4799a1aa693a579
alicianunezisaac <alicianunez10492@gmail.com>
2021-01-23 00:07:25 +0100

    characters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Characters from './components/Characters';
 
 const App = () => {
   return (
     <Fragment>
       <Header title="React App" />
+      <Characters />
     </Fragment>
   );
 };
