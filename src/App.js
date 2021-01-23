f12be7279241f5912721047bf7f94ecd2ff6f5a7
alicianunezisaac <alicianunez10492@gmail.com>
2021-01-23 01:38:54 +0100

    some styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
 import Characters from './components/Characters';
+import Footer from './components/Footer';
+import './main.css';
 
 const App = () => {
   return (
     <Fragment>
       <Header title="Rick and Morty" />
       <Characters />
+      <Footer />
     </Fragment>
   );
 };
